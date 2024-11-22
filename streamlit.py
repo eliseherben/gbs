6dc32145080cc7896b43ff9c811ab595069fd3f1
eliseherben <146279872+eliseherben@users.noreply.github.com>
2024-11-22 14:19:03 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -698,7 +698,7 @@ hersenzenuw = st.selectbox('Uitval/aantasting hersenzenuwen bij binnenkomst', op
 
 # Voorbeeld dataframe met de nieuwe observatie
 input_data = pd.DataFrame({
-    'Age': [st_session_state.Age],
+    'Age': [st.session_state.Age],
     'Sex': [1 if geslacht == 'Vrouw' else 0],
     'MRC_sum_e': [spierkracht_e],
     'MRC_sum_w1': [spierkracht_w1],
